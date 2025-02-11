e9c723227beeba4a83b5e0419be5bc1612007303
Olivier Brochu <olivier.brochu.1@ens.etsmtl.ca>
2025-02-11 11:40:44 -0500

    Update plotSiTi.py and remove unused script

--- src/analysis/plotSiTi.py
+++ src/analysis/plotSiTi.py
@@ -2,6 +2,8 @@ import argparse
 from matplotlib import pyplot
 import pandas
 
+from common import markers
+
 # Arguments
 parser = argparse.ArgumentParser(description="Plots the spatial and temporal information of videos.")
 parser.add_argument("data", help="Path of the CSV file with the spatial and temporal information for each video.")
@@ -13,11 +15,20 @@ args = parser.parse_args()
 frame = pandas.read_csv(args.data)
 frame = frame.groupby("input_file", as_index=False).mean(numeric_only=True)
 
-pyplot.figure()
+pyplot.figure(figsize=(3.25, 2.5))
 
-pyplot.scatter(x="si", y="ti", data=frame)
+i = 0
 
-pyplot.xlabel("Information spatiale moyenne")
-pyplot.ylabel("Information temporelle moyenne")
+for tile in frame["input_file"].unique():
+    series = frame.query("input_file == @tile")
+    marker = markers[i % len(markers)]
 
+    pyplot.scatter(x=series.si, y=series.ti, label=tile, marker=marker, s=50)
+
+    i += 1
+
+pyplot.xlabel("Mean SI")
+pyplot.ylabel("Mean TI")
+
+pyplot.legend(title="Video Name and Tile ID", bbox_to_anchor=(1, 1), loc="upper left", prop={"size": 8}, title_fontsize=8)
 pyplot.savefig(args.figure, bbox_inches="tight")
